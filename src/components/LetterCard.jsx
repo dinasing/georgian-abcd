ad093e94f86cce1baf44fafd556e666617174566
dinasing <dina.sing1212@gmail.com>
2025-09-07 19:04:33 +0300

    feat: replace speechSynthesis with audio files, fix errors in alphabet

--- src/components/LetterCard.jsx
+++ src/components/LetterCard.jsx
@@ -3,21 +3,13 @@ import { useState } from "react";
 export default function LetterCard({ letter }) {
   const [showExample, setShowExample] = useState(false);
 
-  const speak = (text, lang = "ka-GE") => {
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = lang;
-    window.speechSynthesis.speak(utterance);
+  const speak = (audio) => {
+    new Audio(audio).play()
   };
 
   return (
     <div className="p-6 bg-white rounded-2xl shadow-lg text-center">
-      <h1 className="text-6xl font-bold mb-4">{letter.symbol}</h1>
-      <button
-        onClick={() => speak(letter.symbol)}
-        className="px-4 py-2 bg-blue-500 text-white rounded-xl mb-4"
-      >
-        🔊 Слушать букву
-      </button>
+      <h2 className="text-6xl font-bold mb-4">{letter.symbol}</h2>
       <p className="text-xl">Похоже на русскую: {letter.sound}</p>
 
       <button
@@ -33,7 +25,7 @@ export default function LetterCard({ letter }) {
           <p className="text-gray-600">{letter.example.transcription}</p>
           <p className="italic">{letter.example.translation}</p>
           <button
-            onClick={() => speak(letter.example.word)}
+            onClick={() => speak(letter.example.audio)}
             className="mt-2 px-3 py-1 bg-blue-400 text-white rounded-lg"
           >
             🔊 Слушать слово
